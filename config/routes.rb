95fcee9fd5abe7457318167a7fe98169bd5fde81
Phoebe <phoebe.manning8@gmail.com>
2018-05-07 13:28:22 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,5 @@
 Rails.application.routes.draw do
-
   resources :movies
-
-  get 'customers/index'
-
-  get 'customers/show'
-
-  get 'customers/new'
-
-  get 'customers/create'
-
-  get 'customers/destroy'
-
-  get 'customers/edit'
-
-  get 'customers/update'
-
-
+  resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
