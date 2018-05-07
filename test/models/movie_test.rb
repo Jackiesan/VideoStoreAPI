7928265e3a017e94c20503fd2ed4c5a06db861b8
Phoebe <phoebe.manning8@gmail.com>
2018-05-07 13:17:36 -0700

    basic validation tests for movie model

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,32 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  let(:harry_potter) { movies(:harry) }
+
+  describe "validations" do
+    it "is valid when all fields are present" do
+      harry_potter.valid?.must_equal true
+    end
+
+    it "is not valid when title is missing" do
+      harry_potter.title = nil
+      harry_potter.valid?.must_equal false
+    end
+
+    it "is not valid when overview is missing" do
+      harry_potter.overview = nil
+      harry_potter.valid?.must_equal false
+    end
+
+    it "is not valid when release_date is missing" do
+      harry_potter.release_date = nil
+      harry_potter.valid?.must_equal false
+    end
+
+    it "is not valid when inventory is missing" do
+      harry_potter.inventory = nil
+      harry_potter.valid?.must_equal false
+    end
   end
 end
