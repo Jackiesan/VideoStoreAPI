f8b5ccec6c3dfc00eb6ed98866673a2f5b4d4cd6
Jackie <jonofre16@hotmail.com>
2018-05-07 12:49:43 -0700

    Created model and controller customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+
+  get 'customers/show'
+
+  get 'customers/new'
+
+  get 'customers/create'
+
+  get 'customers/destroy'
+
+  get 'customers/edit'
+
+  get 'customers/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
