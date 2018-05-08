d18bb117ed2b59dd20e05646cb594b4bb8f8514b
Phoebe <phoebe.manning8@gmail.com>
2018-05-08 10:30:49 -0700

    rental relation tests

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -2,8 +2,19 @@ require "test_helper"
 
 describe Rental do
   let(:rental) { Rental.new }
+  let(:my_rental) { rentals(:one) }
 
-  it "must be valid" do
-    value(rental).must_be :valid?
+  # it "must be valid" do
+  #   value(rental).must_be :valid?
+  # end
+
+  describe "relations" do
+    it "responds to movies" do
+      my_rental.must_respond_to :movie
+    end
+
+    it "responds to customers" do
+      my_rental.must_respond_to :customer
+    end
   end
 end
