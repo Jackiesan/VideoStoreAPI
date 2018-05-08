93b57507f0e80ce50e5f78f9a59719cc01d2d59d
Phoebe <phoebe.manning8@gmail.com>
2018-05-08 10:53:54 -0700

    checkout and checkin routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :movies
   resources :customers
 
+  post 'rentals/check-out', to: 'rentals#check_out', as: 'check_out'
+  post 'rentals/check-in', to: 'rentals#check_in', as: 'check_in'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
