783ac88026148d1a097b8710094b2b6ec6afc226
Jackie <jonofre16@hotmail.com>
2018-05-08 14:01:52 -0700

    Created basic checkout controller method, added get due date method to rental model

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,7 +1,25 @@
+require 'date'
+
 class RentalsController < ApplicationController
+
   def check_in
   end
 
   def check_out
+    rental = Rental.new(rental_params)
+    rental.checked_out = DateTime.now.to_date
+    rental.due_date = rental.get_due_date
+    rental.check_in_status = false
+    if rental.save
+      render json: { id: rental.id }, status: :ok
+    else
+      render json: { errors: rental.errors }, status: :bad_request
+    end
+  end
+
+  private
+
+  def rental_params
+    return params.permit(:movie_id, :customer_id)
   end
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,7 +3,9 @@ class Rental < ApplicationRecord
   belongs_to :customer
   validates :checked_out, presence: true
   validates :due_date, presence: true
-  validates :movie, presence: true
-  validates :customer, presence: true
-  validates :check_in_status, presence: true
+  validates :check_in_status,  inclusion: { in: [ true, false ] }
+
+  def self.get_due_date
+    return self.check_out + 7
+  end
 end
