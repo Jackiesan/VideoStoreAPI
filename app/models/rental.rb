3bbb61b1cecfd4e073121bac3a60d10135176a07
Jackie <jonofre16@hotmail.com>
2018-05-08 10:25:18 -0700

    Added validations to rental model

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,8 @@
 class Rental < ApplicationRecord
   belongs_to :movie
   belongs_to :customer
+  validates :checked_out, presence: true
+  validates :due_date, presence: true
+  validates :movie, presence: true
+  validates :work, presence: true
 end
