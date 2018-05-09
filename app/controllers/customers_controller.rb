1f5503974be178463acc78e61cd92c4eba34159b
Jackie <jonofre16@hotmail.com>
2018-05-09 15:32:10 -0700

    Added optional query parameters to customers index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,8 @@
 class CustomersController < ApplicationController
   def index
     @customers = Customer.all
+    @customers = @customers.sort_by &params[:sort].to_sym if params[:sort]
+    @customers = @customers.first(params[:n].to_i) if params[:n]
     @customers_data = []
     @customers.each do |customer|
       movies_checked_out_count = {"movies_checked_out_count" => customer.get_count }
